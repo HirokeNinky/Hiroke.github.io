85cc1451e26a08e20ace1a1788388ddf55cf829c
Hiroke Ninky <43679320+HirokeNinky@users.noreply.github.com>
2025-05-04 18:23:04 +0700

    Update firebase-config.js

--- forms/js/firebase-config.js
+++ forms/js/firebase-config.js
@@ -2,12 +2,14 @@ import { firebase.initializeApp } from "firebase/app";
 import { firebase.firestore } from "firebase/analytics";
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDqHJJCa4hbH_Tc9dvqriZ_p41180pKuxA",
-    authDomain: "hiroke-d904d.firebaseapp.com",
-    projectId: "hiroke-d904d",
-    storageBucket: "hiroke-d904d.appspot.com",
-    messagingSenderId: "988494243990",
-    appId: "1:988494243990:web:ec5aafe811ea8c686c7ab7"
+  apiKey: "AIzaSyDqHJJCa4hbH_Tc9dvqriZ_p41180pKuxA",
+  authDomain: "hiroke-d904d.firebaseapp.com",
+  databaseURL: "https://hiroke-d904d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hiroke-d904d",
+  storageBucket: "hiroke-d904d.firebasestorage.app",
+  messagingSenderId: "988494243990",
+  appId: "1:988494243990:web:ec5aafe811ea8c686c7ab7",
+  measurementId: "G-WTGVNQSHHF"
 };
 
 // Initialize Firebase
